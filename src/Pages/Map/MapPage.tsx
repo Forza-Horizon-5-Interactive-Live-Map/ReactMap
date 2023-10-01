5f8b6260438ae7d49587453c9854e639ba290a4f
Antoine Capitain <acapitain@elcia.com>
2023-10-02 00:11:49 +0200

    fix Error for build

--- src/Pages/Map/MapPage.tsx
+++ src/Pages/Map/MapPage.tsx
@@ -1,46 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './MapPage.scss';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { CRS, LatLngBoundsExpression, LatLngExpression } from 'leaflet';
-import MapListener from './MapListener';
 import PlayerList, {
 	folowPlayer as followPlayer,
 } from '../../Component/PlayerList/PlayerList';
 import { MessageDTO } from '../../Services/API/Models/MessageDTO';
 import MapControl from './MapControl';
 import getPlayerList from './PlayerList';
-import useMapSocket from '../../Hook/useMapSocket';
 
 const MapPage = () => {
 	const tilesServerUrl = import.meta.env.VITE_TILE_SERVER;
 	const [followPlayer, setFollowPlayer] = useState<followPlayer>();
 	const playerList = getPlayerList();
-	// const playerList = useMapSocket(import.meta.env.VITE_MAP_SCOKET_URL);
+  // const playerList = useMapSocket(import.meta.env.VITE_MAP_SCOKET_URL);
+  
+const [viewPort, setViewPort] = useState({
+	lat: -128,
+	lng: 128,
+	zoom: 5,
+});
+const [moveCenter, setMoveCenter] = useState<LatLngExpression>([-128, 128]);
 
+const maxBounds: LatLngBoundsExpression = [
+	[-69, 34.75],
+	[-187, 221.2],
+];
+
+const moveMapToCenter = useCallback(
+	(lat: number, lng: number) => {
+		setMoveCenter([lat, lng] as LatLngExpression);
+		setViewPort({ ...viewPort, lat, lng });
+	},
+	[viewPort],
+);
 	useEffect(() => {
 		if (!followPlayer || !followPlayer.ip || !followPlayer.enable) return;
 
 		const followedPlayer = playerList.find(p => p.ip === followPlayer.ip);
 		if (!followedPlayer) return;
 		else moveMapToCenter(followedPlayer.lat, followedPlayer.lng);
-	}, [playerList, followPlayer]);
+	}, [playerList, followPlayer, moveMapToCenter]);
 
-	const [viewPort, setViewPort] = useState({
-		lat: -128,
-		lng: 128,
-		zoom: 5,
-	});
-	const [moveCenter, setMoveCenter] = useState<LatLngExpression>([-128, 128]);
-
-	const maxBounds: LatLngBoundsExpression = [
-		[-69, 34.75],
-		[-187, 221.2],
-	];
-
-	const moveMapToCenter = (lat: number, lng: number) => {
-		setMoveCenter([lat, lng] as LatLngExpression);
-	};
 
 	return (
 		<>
@@ -59,11 +61,6 @@ const MapPage = () => {
 				minZoom={3}
 				maxBounds={maxBounds}
 				crs={CRS.Simple}>
-				<MapListener
-					updateMove={pos =>
-						setViewPort({ ...viewPort, lat: pos.lat, lng: pos.lng })
-					}
-				/>
 				<TileLayer url={`${tilesServerUrl}/{z}/{x}/{y}.png`} />
 				{playerList.map((player: MessageDTO) => (
 					<Marker
