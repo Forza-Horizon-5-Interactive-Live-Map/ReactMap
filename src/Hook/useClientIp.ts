e49608dcbe08a3e3f2d156ec7face8b5164ad6cd
Antoine Capitain <acapitain@elcia.com>
2023-10-11 22:52:26 +0200

    Fix Player Ip

--- src/Hook/useClientIp.ts
+++ src/Hook/useClientIp.ts
@@ -2,13 +2,13 @@ import { useEffect, useState } from "react";
 
 const useClientIp = (): string => {
   const [ip, setIp] = useState('');
-
+  const url: string = 'https://api.ipify.org/?format=json';
 	useEffect(() => {
-    fetch('https://api.ipify.org/?format=json')
+    fetch(url)
       .then((res) => res.json())
-      .then((data) => setIp(data));
+      .then((data) => setIp(data.ip));
 
-	}, []);
+	}, [url]);
 	return ip;
 };
 
